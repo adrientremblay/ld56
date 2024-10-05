aaac091ebc6eec76b30d191001ca96004dc64e75
Adrien Tremblay <adrien@adrientremblay.com>
2024-10-05 19:08:46 -0400

    Decomposition will change appearance of the corpse

--- scripts/corpse.gd
+++ scripts/corpse.gd
@@ -1,6 +1,7 @@
 class_name Corpse extends AnimatedSprite2D
 
 var person_name
+var original_weight
 var weight
 var reward
 
@@ -9,9 +10,17 @@ var reward
 func construct(person_name, weight, reward):
 	self.person_name = person_name
 	self.weight = weight
+	self.original_weight = weight
 	self.reward = reward
 
 func _process(delta: float) -> void:
 	for creature in feeding_area.get_overlapping_bodies():
 		weight -= 0.1
-		print("SUBTRACTING WEIGHT: " + str(weight))
+	
+	set_decomposition_frame()
+
+func set_decomposition_frame():
+	var decomposition_decimal = 1 - (weight / original_weight)
+	var frame_float = 6.9999 * decomposition_decimal
+	var frame_number = floor(frame_float)
+	self.frame = frame_number
