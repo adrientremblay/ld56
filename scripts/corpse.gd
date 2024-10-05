0164811a6ad07993eb4f2ffa7e7d64937ac03329
Adrien Tremblay <adrien@adrientremblay.com>
2024-10-05 19:00:39 -0400

    Creatures reduce the weight of corpses

--- scripts/corpse.gd
+++ scripts/corpse.gd
@@ -4,7 +4,14 @@ var person_name
 var weight
 var reward
 
+@onready var feeding_area: Area2D = $FeedingArea
+
 func construct(person_name, weight, reward):
 	self.person_name = person_name
 	self.weight = weight
 	self.reward = reward
+
+func _process(delta: float) -> void:
+	for creature in feeding_area.get_overlapping_bodies():
+		weight -= 0.1
+		print("SUBTRACTING WEIGHT: " + str(weight))
