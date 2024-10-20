92b85c40dadb1303b114ce715189d8f6cc8acf08
Adrien Tremblay <adrien@adrientremblay.com>
2024-10-20 16:57:16 -0400

    Got the different emotions tow ork for the spokesman

--- scenes/spokesman_portrait.gd
+++ scenes/spokesman_portrait.gd
@@ -1,1 +1,16 @@
 extends DialogicPortrait
+func _update_portrait(passed_character:DialogicCharacter, passed_portrait:String) -> void:
+	print (passed_portrait)
+	if (passed_portrait == "Frowning"):
+		$Sprite.play("frowning")
+	elif (passed_portrait == "Smiling"):
+		$Sprite.play("smiling")
+	else:
+		$Sprite.play("deadfaced")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	#$Sprite.play()
+	pass
+
+func _get_covered_rect() -> Rect2:
+	return Rect2($Sprite.position, $Sprite.sprite_frames.get_frame_texture($Sprite.animation, 0).get_size()*$Sprite.scale)
