92d09926a0b65e200316b7f14953063549198d7b
Adrien Tremblay <adrien@adrientremblay.com>
2024-10-21 21:48:43 -0400

    Cannot manually pause when the dialogic timeline is running

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -5,7 +5,7 @@ func _ready() -> void:
 	self.visible = false
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause") && Dialogic.current_timeline == null:
 		get_tree().paused = ! get_tree().paused
 		if get_tree().paused:
 			self.visible = true
