a8d4edf7ef0b0fde318174a55ae9b38d92070524
Adrien Tremblay <adrien@adrientremblay.com>
2024-10-21 21:29:12 -0400

    Implemented the actual visuals to the pause menu panel

--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -2,7 +2,12 @@ extends Panel
 
 func _ready() -> void:
 	self.process_mode = Node.PROCESS_MODE_ALWAYS
+	self.visible = false
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		get_tree().paused = ! get_tree().paused
+		if get_tree().paused:
+			self.visible = true
+		else:
+			self.visible = false
