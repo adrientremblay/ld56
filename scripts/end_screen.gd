ec44f4973355fc3d1e17da110cb26d642e46c2ff
Adrien Tremblay <adrien@adrientremblay.com>
2024-10-24 21:05:52 -0400

    Created passable endgame screen

--- scripts/end_screen.gd
+++ scripts/end_screen.gd
@@ -5,15 +5,16 @@ func _ready() -> void:
 
 func open(corpses_disposed: int):
 	self.visible = true
-	$Spokesman.play()
-	if (corpses_disposed < 5):
-		$Label.text = "You have dissapointed us greatly. Your aquarium is ruined and we have many more bodies to dispose of. It will be a pain to dispose of yours."
-		$SpokesmanSpeechNo.play()
-	elif (corpses_disposed < 10):
-		$Label.text = "You have done satisfactory. Set up another tank and we will be in contact."
-		$SpokesmanSpeechNormal.play()
-	else:
-		$Label.text = "You have not let us down. No one on earth besides us know where these people are. We look forward to working with you again."
-		$SpokesmanSpeechNormal.play()
+	
+	#$Spokesman.play()
+	#if (corpses_disposed < 5):
+		#$Label.text = "You have dissapointed us greatly. Your aquarium is ruined and we have many more bodies to dispose of. It will be a pain to dispose of yours."
+		#$SpokesmanSpeechNo.play()
+	#elif (corpses_disposed < 10):
+		#$Label.text = "You have done satisfactory. Set up another tank and we will be in contact."
+		#$SpokesmanSpeechNormal.play()
+	#else:
+		#$Label.text = "You have not let us down. No one on earth besides us know where these people are. We look forward to working with you again."
+		#$SpokesmanSpeechNormal.play()
 	
 	$FinalScoreLabel.text = "Corpses Disposed: " + str(corpses_disposed)
