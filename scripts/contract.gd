e14ad904215aec505fd9cae03ab622b0ce06136e
Adrien Tremblay <adrien@adrientremblay.com>
2024-10-27 21:02:11 -0400

    added game factions as a dictionary

--- scripts/contract.gd
+++ scripts/contract.gd
@@ -1,12 +1,44 @@
 class_name Contract extends PanelContainer
 
 enum Appearance {THIN, MEDIUM, OBESE}
+
 var appearance: Appearance
 
 var person_name
 var weight
 var reward
 var female
+
+var organizations = [
+	{
+		"name" : "PureGenus",
+		"description1" : "They believe that the average human genetic composition has become inferior",
+		"description2" : "Kidnap people they believe have inferior genes and forcefully sterilise them. There are often fatalities in the struggle",
+		"description3" : "There is a heavy racial bias in determining if an individual is 'inferior' or not",
+		"karma" : -0.3
+	},
+	{
+		"name" : "Blackstone Pharmaceuticals",
+		"description1" : "Performs human experiments on people",
+		"description2" : "Sells drugs at exorbitant markups",
+		"description3" : "Markets unnecessary drugs to vulnerable groups of people",
+		"karma" : -0.2
+	},
+	{
+		"name" : "The Reclamation Network",
+		"description1" : "Anonymously donate organs to hospitals around the globe ultimately saving lives",
+		"description2" : "Secretly sources their organs from proven rapists and pedophiles both in and out of prison",
+		"description3" : "Comprised primarily by ex law enforcement",
+		"karma" : 0.2
+	},
+	{
+		"name" : "Project Horizon",
+		"description1" : "Performs experimental treatments on willing volunteers who are infected with fatal diseases",
+		"description2" : "Their activity is illegal",
+		"description3" : "Wish to dispose of bodies to avoid detection and public scrutiny",
+		"karma" : 0.3
+	},
+]
 
 @onready var name_label : Label = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/NameLabel
 @onready var weight_label = $MarginContainer/VBoxContainer/HBoxContainer/VBoxContainer/WeightLabel
