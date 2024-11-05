2f2f51d3240bd698f5a5412f117f97737a048516
Adrien Tremblay <adrien@adrientremblay.com>
2024-11-05 08:55:00 -0500

    Adjusted the numbers for all the creatures

--- scripts/creature.gd
+++ scripts/creature.gd
@@ -13,40 +13,62 @@ var lobster_sprite_scene : PackedScene = load("res://scenes/lobster_sprite.tscn"
 var octopus_sprite_scene : PackedScene = load("res://scenes/octopus_sprite.tscn")
 var turtle_sprite_scene : PackedScene = load("res://scenes/turtle_sprite.tscn")
 
+var price
 var damage
 var speed
 
+# The value per dollar (VPD) of a creature it it's (damange * speed) / price
 func set_species(species: Species):
 	var sprite
 	match species:
-		Species.SNAIL:
+		Species.SNAIL: # vpd = 0.5
 			sprite = snail_sprite_scene.instantiate()
-			self.damage = 2
-		Species.CRAB:
+			self.price = 5
+			self.damage = 2.5
+			self.speed = 1
+		Species.CRAB: #vpd = 0.8
 			sprite = crab_sprite_scene.instantiate()
-			self.damage = 5
-		Species.KILLIFISH:
-			sprite = killifish_sprite_scene.instantiate()
-			self.damage = 3
-		Species.PIRANHA:
-			sprite = piranha_sprite_scene.instantiate()
-			self.damage = 5
-		Species.ANGLERFISH:
-			sprite = anglerfish_sprite_scene.instantiate()
-			self.damage = 5
-		Species.DRAGONFISH:
-			sprite = dragonfish_sprite_scene.instantiate()
-			self.damage = 5
-		Species.GOBLINSHARK:
-			sprite = goblinshark_sprite_scene.instantiate()
-			self.damage = 20
-		Species.LOBSTER:
+			self.price = 25
+			self.damage = 8
+			self.speed = 2.5
+		Species.LOBSTER: # vpd = 1.0
 			sprite = lobster_sprite_scene.instantiate()
-			self.damage = 10
-		Species.OCTOPUS:
+			self.price = 50
+			self.damage = 25
+			self.speed = 2
+		Species.OCTOPUS: # vpd = 1.75
 			sprite = octopus_sprite_scene.instantiate()
-			self.damage = 15
-		Species.TURTLE:
+			self.price = 300
+			self.damage = 150
+			self.speed = 3.5
+		Species.TURTLE: #vpd = 2.8
 			sprite = turtle_sprite_scene.instantiate()
-			self.damage = 30
+			self.price = 500
+			self.damage = 700
+			self.speed = 2
+		Species.KILLIFISH: # vpd = 0.5
+			sprite = killifish_sprite_scene.instantiate()
+			self.price = 5
+			self.damage = 1
+			self.speed = 2.5
+		Species.PIRANHA: # vpd = 0.8
+			sprite = piranha_sprite_scene.instantiate()
+			self.price = 25
+			self.damage = 4
+			self.speed = 5
+		Species.ANGLERFISH: # vpd = 1.0
+			sprite = anglerfish_sprite_scene.instantiate()
+			self.price = 50
+			self.damage = 12.5
+			self.speed = 4
+		Species.DRAGONFISH: # vpd = 1.75
+			sprite = dragonfish_sprite_scene.instantiate()
+			self.price = 300
+			self.damage = 75
+			self.speed = 7
+		Species.GOBLINSHARK: # vpd = 2.8
+			sprite = goblinshark_sprite_scene.instantiate()
+			self.price = 500
+			self.damage = 350
+			self.speed = 4
 	add_child(sprite)
