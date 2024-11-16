45c47aa63f862c899e3fab24100d4acb22f78a5a
Adrien Tremblay <adrien@adrientremblay.com>
2024-11-15 20:18:33 -0500

    Created buy filter menu and implemented ability to open and close it

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -1,37 +1,50 @@
 extends Control
 
+@onready var contract_menu: Panel = $ContractMenu
 @onready var creature_menu: Panel = $CreatureMenu
+@onready var plant_menu: Panel = $PlantMenu
+@onready var filter_menu: Panel = $FilterMenu
 
 signal contract_menu_opened
 
 func _ready() -> void:
 	creature_menu.visible = false
 	self.process_mode = Node.PROCESS_MODE_ALWAYS
+	close_all_windows()
 
 func _on_creature_button_pressed() -> void:
+	close_all_windows()
 	creature_menu.visible = !creature_menu.visible
 
 func _on_contract_menu_contract_menu_opened() -> void:
 	# close all other UI
-	creature_menu.visible = false
-	$PlantMenu.visible = false
+	close_all_windows()
+	contract_menu.visible = true
 	contract_menu_opened.emit()
 
 func _process(delta: float) -> void:
 	var time_left = floor($ContractMenu/NewContractTimer.time_left)
-
 	$NextContractLabel.text = "New Contracts: " + str(time_left)
 
 func _on_close_creature_menu_pressed() -> void:
 	creature_menu.visible = false
 
 func _on_close_plant_menu_pressed() -> void:
-	$PlantMenu.visible = false
+	plant_menu.visible = false
 
 func _on_plants_button_pressed() -> void:
-	$PlantMenu.visible = ! $PlantMenu.visible
+	close_all_windows()
+	plant_menu.visible = ! plant_menu.visible
 
 func close_all_windows():
-	$CreatureMenu.visible = false
-	$PlantMenu.visible = false
-	$ContractMenu.visible = false
+	creature_menu.visible = false
+	plant_menu.visible = false
+	contract_menu.visible = false
+	filter_menu.visible = false
+
+func _on_close_filter_menu_pressed() -> void:
+	filter_menu.visible = false
+
+func _on_filters_button_pressed() -> void:
+	close_all_windows()
+	filter_menu.visible = !filter_menu.visible
