be93fe3e228f567579bc4ce25669cdb37087020a
Adrien Tremblay <adrien@adrientremblay.com>
2024-11-15 20:27:52 -0500

    Fixed menus

--- scripts/UI.gd
+++ scripts/UI.gd
@@ -12,10 +12,6 @@ func _ready() -> void:
 	self.process_mode = Node.PROCESS_MODE_ALWAYS
 	close_all_windows()
 
-func _on_creature_button_pressed() -> void:
-	close_all_windows()
-	creature_menu.visible = !creature_menu.visible
-
 func _on_contract_menu_contract_menu_opened() -> void:
 	# close all other UI
 	close_all_windows()
@@ -32,19 +28,41 @@ func _on_close_creature_menu_pressed() -> void:
 func _on_close_plant_menu_pressed() -> void:
 	plant_menu.visible = false
 
+func _on_close_filter_menu_pressed() -> void:
+	filter_menu.visible = false
+
+func _on_creature_button_pressed() -> void:
+	if contract_menu.visible:
+		return
+	
+	if creature_menu.visible:
+		creature_menu.visible = false
+	else:
+		close_all_windows()
+		creature_menu.visible = !creature_menu.visible
+
 func _on_plants_button_pressed() -> void:
-	close_all_windows()
-	plant_menu.visible = ! plant_menu.visible
+	if contract_menu.visible:
+		return
+	
+	if plant_menu.visible:
+		plant_menu.visible = false
+	else:
+		close_all_windows()
+		plant_menu.visible = true
+
+func _on_filters_button_pressed() -> void:
+	if contract_menu.visible:
+		return
+	
+	if filter_menu.visible:
+		filter_menu.visible = false
+	else:
+		close_all_windows()
+		filter_menu.visible = !filter_menu.visible
 
 func close_all_windows():
 	creature_menu.visible = false
 	plant_menu.visible = false
 	contract_menu.visible = false
 	filter_menu.visible = false
-
-func _on_close_filter_menu_pressed() -> void:
-	filter_menu.visible = false
-
-func _on_filters_button_pressed() -> void:
-	close_all_windows()
-	filter_menu.visible = !filter_menu.visible
