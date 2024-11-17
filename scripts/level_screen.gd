496cabc4d7efc1a37900a06ca7ecc8f8ed230ce3
Adrien Tremblay <adrien@adrientremblay.com>
2024-11-16 19:48:26 -0500

    started improving level screen

--- scripts/level_screen.gd
+++ scripts/level_screen.gd
@@ -10,14 +10,18 @@ func open(level_number: int, tagline: String, quota: int, bonus: int, penalty: i
 	self.visible = true
 	$LevelLabel.text = "Level " + str(level_number)
 	$TaglineLabel.text = tagline
-	$QuotaLabel.text = "Quota: " + str(quota) + " bodies"
+	$QuotaLabel.text = "Quota: " + str(quota) + (" bodies" if level_number != 1 else " body")
 	if bonus != 0:
 		$BonusLabel.text = "You received a bonus of " + str(bonus) + "$ for closing early."
+	elif level_number == 1:
+		$BonusLabel.text = ""
 	else:
 		$BonusLabel.text = "You did not receive a bonus."
 	
 	if penalty != 0:
 		$PenaltyLabel.text = "You were penalized " + str(penalty) + "$ for failing to dispose of all bodies."
+	elif level_number == 1:
+		$PenaltyLabel.text = ""
 	else:
 		$PenaltyLabel.text = "You were not penalized."
 
