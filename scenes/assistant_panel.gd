a8ece04d899f74edce1acb02dbdfd1083dce156a
Adrien Tremblay <adrien@adrientremblay.com>
2024-11-28 00:09:50 -0500

    add an ANNOYING SOUND when the assistant spawns in

--- scenes/assistant_panel.gd
+++ scenes/assistant_panel.gd
@@ -13,6 +13,7 @@ func open(message: String):
 	message_label.text = message
 	self.visible = true
 	animation_player.play("open_animation")
+	$AssistantSound.play()
 
 func open_ammonia_warning():
 	self.open("The ammonia level is getting dangerous! You should probably buy a filter to reduce it!")
